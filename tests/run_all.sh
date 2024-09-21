39984bbda29e0092b4e331b61296af552a3471a0
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-09-20 22:27:17 -0400

    vimqq: test discovery

---
+++ tests/run_all.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# vimqq version/implementation to test
+vimqq_path=$1
+if [ -z "$vimqq_path" ]; then
+  echo "Usage: $0 <vimqq path>"
+  exit 1
+fi
+
+# Expand the relative path to an absolute path
+vimqq_path=$(realpath "$vimqq_path")
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# iterate over all *.vim input scripts
+for script in "$script_dir"/*.vim; do
+    testname="$(basename "$script")"
+    testname="${testname%.vim}"
+    tmp_file="$(mktemp)"
+
+    "$script_dir"/vimqq_test.sh "$vimqq_path" "$testname" > "$tmp_file" 2>&1
+
+    exit_code=$?
+
+    result="\033[0;31m[fail]\033[0m"
+    if [ $exit_code -eq 0 ]; then
+        result="\033[0;32m[ ok ]\033[0m"
+    fi
+    echo -e "$result $testname"
+    rm "$tmp_file"
+done
--- tests/test_qq_list.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-"$script_dir"/vimqq_test.sh "$1" qq_list
