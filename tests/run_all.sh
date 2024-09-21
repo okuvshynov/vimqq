9e11ab7f7e290779a6f49e62097897b2c9bd769e
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-09-20 22:39:31 -0400

    vimqq: run all fixes

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -12,20 +12,31 @@ vimqq_path=$(realpath "$vimqq_path")
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+n_failed=0
 # iterate over all *.vim input scripts
 for script in "$script_dir"/*.vim; do
     testname="$(basename "$script")"
     testname="${testname%.vim}"
     tmp_file="$(mktemp)"
 
-    "$script_dir"/vimqq_test.sh "$vimqq_path" "$testname" > "$tmp_file" 2>&1
+    "$script_dir"/vimqq_test.sh "$vimqq_path" "$testname"
 
     exit_code=$?
 
     result="\033[0;31m[fail]\033[0m"
     if [ $exit_code -eq 0 ]; then
         result="\033[0;32m[ ok ]\033[0m"
+    else
+        ((n_failed++))
     fi
     echo -e "$result $testname"
     rm "$tmp_file"
 done
+
+echo "---------------------------------"
+if [ $n_failed -gt 0 ]; then
+    echo -e "\033[0;31m[fail]\033[0m $n_failed tests failed."
+    exit 1
+else
+    echo -e "\033[0;32m[ ok ]\033[0m all tests succeeded."
+fi
