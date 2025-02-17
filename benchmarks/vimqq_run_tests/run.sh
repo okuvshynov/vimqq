712e67cb5ebe6c7231f2ef82abc98bbb765a9d09
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2025-02-16 23:50:24 -0500

    vimqq: move more to dockerfile

--- benchmarks/vimqq_run_tests/run.sh
+++ benchmarks/vimqq_run_tests/run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e  # Exit on any error
 
-# install flask for mock server
-pip3 install flask
-
-# install themis for vim testing
-mkdir -p ~/.vim/pack/plugins/start/vim-themis
-git clone https://github.com/thinca/vim-themis.git ~/.vim/pack/plugins/start/vim-themis/
-export PATH="~/.vim/pack/plugins/start/vim-themis/bin:$PATH"
-
 # clone vimqq
 git clone https://github.com/okuvshynov/vimqq.git
 
