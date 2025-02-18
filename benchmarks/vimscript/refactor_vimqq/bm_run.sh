1957f0d517b37383c47855f11e7e422433a9016b
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2025-02-18 14:43:59 -0500

    vimqq: cleanup a little

--- benchmarks/vimscript/refactor_vimqq/bm_run.sh
+++ benchmarks/vimscript/refactor_vimqq/bm_run.sh
@@ -3,12 +3,16 @@ set -e  # Exit on any error
 
 cd /app/vimqq/
 
+# Running vimqq 
 vim -u /app/vimrc -c 'QQI @sonnet after several refactor changes, main.vim and cmd.vim can be merged. Do the merge and make corresponding changes in vimqq.vim file as well'
 
+# imprerfect verification
+
+# verify that at least one file was deleted
 if ! git status --porcelain | grep -q "^ D"; then
     echo "No deleted file found"
     exit 1
 fi
 
-# verification steps will be here
+# run existing unit tests
 themis tests/local
